ce8691fd90378b26f4b6ab4f6d66b172070b4848
Alexey Rott <survare@hotmail.com>
2019-07-09 14:17:18 +0300

    Date.add done

--- app/src/main/java/ru/skillbranch/devintensive/extentions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extentions/Date.kt
@@ -6,3 +6,18 @@ import java.util.*
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     return SimpleDateFormat(pattern, Locale.forLanguageTag("ru")).format(this)
 }
+
+enum class TimeUnits(val v: Int) {
+    SECOND(1000),
+    MINUTE(SECOND.v*60),
+    HOUR(MINUTE.v*60),
+    DAY(HOUR.v*24)
+}
+
+fun Date.add(
+    value: Int,
+    timeUnit: TimeUnits
+): Date {
+    this.time = this.time + timeUnit.v*value
+    return this
+}
