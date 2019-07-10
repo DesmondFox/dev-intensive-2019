36646fc950a1a1f9046f1e133bcc9d0e1a0ffa7e
Alexey Rott <survare@hotmail.com>
2019-07-10 18:47:44 +0300

    [fix attempt] renamed package

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,4 +1,4 @@
-package ru.skillbranch.devintensive.extentions
+package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
@@ -7,12 +7,18 @@ fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
     return SimpleDateFormat(pattern, Locale.forLanguageTag("ru")).format(this)
 }
 
+//const val sec = 1000
+//const val min = 60*sec
+//const val hour = 60*min
+//const val day = 24*hour
+
 enum class TimeUnits(val v: Int) {
     SECOND(1000),
     MINUTE(SECOND.v*60),
     HOUR(MINUTE.v*60),
     DAY(HOUR.v*24)
 }
+
 
 fun Date.add(
     value: Int,
