e0ef95def6913640c205b1ea6edfe7ae4cee7be3
Alexey Rott <survare@hotmail.com>
2019-07-11 14:37:39 +0300

    toInitials done

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -16,7 +16,14 @@ object Utils {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    fun toInitials(firstName: String?, lastName: String?): String {
-        return ""
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        fun valid(str: String?) = str != null && str != "" && str != " "
+        fun getFirst(str: String) = str[0].toUpperCase()
+
+        var initials = ""
+        initials += if (valid(firstName)) getFirst(firstName!!) else ""
+        initials += if (valid(lastName)) getFirst(lastName!!) else ""
+
+        return if (initials == "") null else initials
     }
 }
