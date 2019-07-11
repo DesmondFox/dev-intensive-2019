349fff379b973132f83a9864f309e86c7026b636
Alexey Rott <survare@hotmail.com>
2019-07-11 17:03:47 +0300

    truncate done

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,2 +1,21 @@
 package ru.skillbranch.devintensive.extensions
 
+fun String.truncate(visibleCount: Int = 16): String {
+    var truncated = this.replace(Regex("""\A\W+"""), "")
+    truncated = truncated.take(visibleCount)
+
+    var trCount = 0
+    var refCount = 0
+    for ((i, c) in this.withIndex()) {
+        if (c != ' ')
+            refCount++
+
+        if (i < truncated.length && c != ' ')
+            trCount++
+    }
+    truncated = truncated.replace(Regex("""\s+$"""), "")
+
+    // Если исходного ПОЛЕЗНОГО текста было больше чем обрезанного, тогда добавляем "..."
+    truncated += if (trCount < refCount) "..." else ""
+    return truncated
+}
