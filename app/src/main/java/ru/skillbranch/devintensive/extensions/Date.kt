75cb111996fdc3f31c37121becc846ea1f91cd78
Alexey Rott <survare@hotmail.com>
2019-07-11 16:00:41 +0300

    plural done

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -14,9 +14,26 @@ fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
 
 enum class TimeUnits(val v: Int) {
     SECOND(1000),
-    MINUTE(SECOND.v*60),
-    HOUR(MINUTE.v*60),
-    DAY(HOUR.v*24)
+    MINUTE(SECOND.v * 60),
+    HOUR(MINUTE.v * 60),
+    DAY(HOUR.v * 24),
+    YEAR(DAY.v * 365);
+
+    fun plural(v: Int): String {
+        val form1 = listOf("секунд", "минут", "часов", "дней", "лет")
+        val form2 = listOf("секунду", "минуту", "час", "день", "год")
+        val form3 = listOf("секунды", "минуты", "часа", "дня", "года")
+
+        val lastNumber = v % 10
+        val penultimateNumber = (v % 100 - lastNumber) / 10
+        return "$v ${
+            when {
+                penultimateNumber == 1 -> form1[this.ordinal]
+                lastNumber == 0 || lastNumber in 5..9 -> form1[this.ordinal]
+                lastNumber in 2..4 -> form3[this.ordinal]
+                else -> form2[this.ordinal]
+            }}"
+    }
 }
 
 
@@ -24,6 +41,6 @@ fun Date.add(
     value: Int,
     timeUnit: TimeUnits
 ): Date {
-    this.time = this.time + timeUnit.v*value
+    this.time = this.time + timeUnit.v * value
     return this
 }
