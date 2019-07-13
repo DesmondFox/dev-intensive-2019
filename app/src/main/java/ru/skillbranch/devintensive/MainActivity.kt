2bcb861732a87740e8a3eda94804c60834943b7b
Alexey Rott <survare@hotmail.com>
2019-07-13 21:49:01 +0300

    copy-pasted code from lesson

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,16 +1,54 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var benderObj = Bender()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        savedInstanceState?.let {
+            val status = it.getString("STATUS") ?: Bender.Status.NORMAL.name
+            val question = it.getString("QUESTION") ?: Bender.Question.NAME.name
+            benderObj.question = Bender.Question.valueOf(question)
+            benderObj.status = Bender.Status.valueOf(status)
+            val color = benderObj.status.color
+            iv_bender.setColorFilter(
+                    Color.rgb(color.first, color.second, color.third),
+                    PorterDuff.Mode.MULTIPLY)
+        }
+        tv_text.text = benderObj.askQuestion()
+        iv_send.setOnClickListener(this)
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString("STATUS", benderObj.status.name)
+        outState?.putString("QUESTION", benderObj.question.name)
+        Log.d(javaClass.name, "onSaveInstanceState")
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.iv_send) {
+            val (phrase, color) = benderObj.listenAnswer(et_message.text.toString())
+            et_message.text.clear()
+            iv_bender.setColorFilter(
+                    Color.rgb(color.first, color.second, color.third),
+                    PorterDuff.Mode.MULTIPLY)
+            tv_text.text = phrase
+        }
     }
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,53 @@
+package ru.skillbranch.devintensive.models
+
+class Bender(
+        var status: Status = Status.NORMAL,
+        var question: Question = Question.NAME
+) {
+
+    fun askQuestion(): String = question.question
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        return if (question.answers.contains(answer.toLowerCase())) {
+            question = question.nextQuestion()
+            "Отлично - это правильный ответ\n${question.question}" to status.color
+        } else {
+            status = status.nextStatus()
+            "Это неправильный ответ\n${question.question}" to status.color
+        }
+    }
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)),
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 255, 0));
+
+        fun nextStatus(): Status = values()[(this.ordinal+1) % values().size]
+    }
+
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+            override fun nextQuestion() = PROFESSION
+        },
+        PROFESSION("Назови мою професию?", listOf("сгибальщик", "bender")){
+            override fun nextQuestion() = MATERIAL
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")){
+            override fun nextQuestion() = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993")){
+            override fun nextQuestion() = SERIAL
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057")){
+            override fun nextQuestion() = IDLE
+        },
+        IDLE ("На этом всё, больше вопросов нет", listOf()){
+            override fun nextQuestion() = IDLE
+        };
+
+        abstract fun nextQuestion(): Question
+    }
+
+
+}
