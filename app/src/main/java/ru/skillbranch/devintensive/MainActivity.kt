9c732b920002e47f7ca01647064c7b8fd8499de6
Alexey Rott <survare@hotmail.com>
2019-07-13 12:54:56 +0300

    Initial commit

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,16 @@
 package ru.skillbranch.devintensive
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
