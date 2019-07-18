ceff84da6f9574fa0ca3f7f0814ac82762214543
Alexey Rott <survare@hotmail.com>
2019-07-18 12:08:29 +0300

    validation check done

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -1,13 +1,17 @@
 package ru.skillbranch.devintensive.models
 
 class Bender(
-        var status: Status = Status.NORMAL,
-        var question: Question = Question.NAME
+    var status: Status = Status.NORMAL,
+    var question: Question = Question.NAME
 ) {
 
     fun askQuestion(): String = question.question
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        if (!question.validate(answer)) {
+            return "${question.invalidCause}\n${question.question}" to status.color
+        }
+
         return if (question.answers.contains(answer.toLowerCase())) {
             question = question.nextQuestion()
             "Отлично - ты справился\n${question.question}" to status.color
@@ -32,30 +36,72 @@ class Bender(
         DANGER(Triple(255, 60, 60)),
         CRITICAL(Triple(255, 0, 0));
 
-        fun nextStatus(): Status = values()[(this.ordinal+1) % values().size]
+        fun nextStatus(): Status = values()[(this.ordinal + 1) % values().size]
     }
 
     enum class Question(val question: String, val answers: List<String>) {
         NAME("Как меня зовут?", listOf("бендер", "bender")) {
             override fun nextQuestion() = PROFESSION
+
+            override val invalidCause: String
+                get() = "Имя должно начинаться с заглавной буквы"
+
+            override fun validate(answer: String): Boolean {
+                return if (answer.isNotEmpty()) answer[0].isUpperCase() else false
+            }
         },
-        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")){
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
             override fun nextQuestion() = MATERIAL
+
+            override val invalidCause: String
+                get() = "Профессия должна начинаться со строчной буквы"
+
+            override fun validate(answer: String): Boolean {
+                return if (answer.isNotEmpty()) answer[0].isLowerCase() else false
+            }
         },
-        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")){
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
             override fun nextQuestion() = BDAY
+
+            override val invalidCause: String
+                get() = "Материал не должен содержать цифр"
+
+            override fun validate(answer: String): Boolean {
+                return !answer.contains(Regex("""[0-9]"""))
+            }
         },
-        BDAY("Когда меня создали?", listOf("2993")){
+        BDAY("Когда меня создали?", listOf("2993")) {
             override fun nextQuestion() = SERIAL
+
+            override val invalidCause: String
+                get() = "Год моего рождения должен содержать только цифры"
+
+            override fun validate(answer: String): Boolean {
+                return !answer.contains(Regex("""[^0-9]"""))
+            }
         },
-        SERIAL("Мой серийный номер?", listOf("2716057")){
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion() = IDLE
+
+            override val invalidCause: String
+                get() = "Серийный номер содержит только цифры, и их 7"
+
+            override fun validate(answer: String): Boolean {
+                return !answer.contains(Regex("""[^0-9]""")) && answer.length == 7
+            }
         },
-        IDLE ("На этом все, вопросов больше нет", listOf()){
+        IDLE("На этом все, вопросов больше нет", listOf()) {
             override fun nextQuestion() = IDLE
+
+            override val invalidCause: String
+                get() = ""
+
+            override fun validate(answer: String): Boolean = true
         };
 
         abstract fun nextQuestion(): Question
+        abstract fun validate(answer: String): Boolean
+        abstract val invalidCause: String
     }
 
 
