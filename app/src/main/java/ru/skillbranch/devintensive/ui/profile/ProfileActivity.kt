e76870e361df7f0c8bff6140b8a664d77b88130a
Alexey <survare@hotmail.com>
2019-08-17 08:46:50 +0300

    updated

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,12 +1,18 @@
 package ru.skillbranch.devintensive.ui.profile
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        iv_avatar.setOnClickListener {
+            Toast.makeText(this, "Test", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
